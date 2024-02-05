fb43764e4678b1ee25eb67f68f86508043166f32
Trung <50526463+trungle15@users.noreply.github.com>
2024-02-04 18:00:33 -0600

    Add constructors and basic methods

--- BigFraction.java
+++ BigFraction.java
@@ -1,3 +1,42 @@
+import java.math.BigInteger;
+
 public class BigFraction {
-  
+  // Fields
+  private BigInteger num;
+  private BigInteger denom;
+
+  // Constructors 
+
+  // ---- Constructor for (BigInteger, BigInteger) 
+  public BigFraction(BigInteger num, BigInteger denom) {
+    this.num = num;
+    this.denom = denom;
+  }
+
+  // ---- Constructor for (int, int) 
+  public BigFraction(int num, int denom) {
+    this.num = BigInteger.valueOf(num);
+    this.denom = BigInteger.valueOf(denom);
+  }
+
+  // --- Constructor for (String) --- Parsing
+  public BigFraction(String fraction) {
+    // To add input validation later??
+    int divisorIndex = fraction.indexOf('/');
+
+    String numArray = fraction.substring(0, divisorIndex);
+    String denomArray = fraction.substring(divisorIndex + 1);
+
+    this.num = new BigInteger(numArray);
+    this.denom = new BigInteger(denomArray);
+  }
+
+  // Methods
+  public BigInteger getNumerator() {
+    return num;
+  }
+
+  public BigInteger getDenominator() {
+    return denom;
+  }
 }
