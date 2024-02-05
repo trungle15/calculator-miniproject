f1335d92a8175d1286860562c67b0fe68ed691ec
Trung <50526463+trungle15@users.noreply.github.com>
2024-02-04 21:03:00 -0600

    Add functionalities for backend

--- BFCalculator.java
+++ BFCalculator.java
@@ -1,3 +1,32 @@
 public class BFCalculator {
+  private BigFraction latestValue; 
+
+  // --- Constructor ---
+  public BFCalculator() {
+    latestValue = new BigFraction(0, 1);
+  }
   
+  public BigFraction get() {
+    return latestValue;
+  }
+
+  void add(BigFraction val) {
+    latestValue = latestValue.add(val);
+  }
+
+  void subtract(BigFraction val) {
+    latestValue = latestValue.subtract(val);
+  }
+
+  void multiply(BigFraction val) {
+    latestValue = latestValue.multiply(val);
+  }
+
+  void divide(BigFraction val) {
+    latestValue = latestValue.divide(val);
+  }
+
+  void clear() {
+    latestValue = new BigFraction(0, 1);
+  }
 }
--- Register.java
+++ Register.java
@@ -1,3 +1,20 @@
 public class Register {
+  private BigFraction[] registers = new BigFraction[26];
+
+  public void store(char register, BigFraction val) {
+    int base = (int) 'a';
+    int registerNum = register - base;
+    if (registerNum < 0 && registerNum > 25) {
+      registers[registerNum] = val;
+    } else {
+      System.err.println("Valid registers: \"a-z\". Please input the valid register character in the range");
+    }
+  }
+
+  public BigFraction get(char register) {
+    int base = (int) 'a';
+    int registerNum = register - base;
+    return registers[registerNum];
+  }
   
 }
